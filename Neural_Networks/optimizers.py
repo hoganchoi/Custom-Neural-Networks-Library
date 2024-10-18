12f2d4ffcc6bf4b4b021e7479290fdfaa3d79a5d
hoganchoi <131487154+hoganchoi@users.noreply.github.com>
2024-10-18 19:50:31 -0400

    Update optimizers.py
    Updated SGD so that it doesn't need to take in parameters when initailizing.

--- Neural_Networks/optimizers.py
+++ Neural_Networks/optimizers.py
@@ -15,32 +15,29 @@ class SGD():
     A class that contains the SGD optimzer used for training the model.
 
     Attributes:
-        activation (activations): The activation function used in the fully connected layer.
-        lr (float): The learning rate for optimizer.
+        None
 
     Methods:
-        optimzeGrad (self, x, input, weights, bias, out_delta): Performs the backward pass using SGD optmization method.
+        optimzeGrad (self, x, input, weights, bias, activation, out_delta, lr): Performs the backward pass using SGD optmization method.
 
     Usage:
         Used to train a fully connected layer through Stochastic Gradient Descent.
     '''
-    ## Initializes the SGD optimizer on CPU.
-    def __init__(self, activation, lr):
+    ## Initializes the SGD function on the CPU.
+    def __init__(self):
         '''
-        Initializes the parameters for SGD optimzer.
+        Initializes the parameters for SGD function.
 
         Args:
-            activation (activations): The activation used on the fully connected layers.
-            lr (float): The learning rate of the optimzer.
+            None
 
         Returns:
             None
         '''
-        self.activation = activation
-        self.lr = lr
+        pass
 
     ## Performs the optimization process.
-    def optimzeGrad(self, x, input, weights, bias, out_delta):
+    def optimizeGrad(self, x, input, weights, bias, activation, out_delta, lr):
         '''
         Performs SGD optimization, updating the weights and biases as well as find the error for the previous layer.
 
@@ -49,7 +46,9 @@ class SGD():
             input (np.array): The input for this layer.
             weights (np.array): The array representing the weights.
             bias (np.array): The array representing the biases.
+            activation (activations): The activation used on the fully connected layers.
             out_delta (np.array): The array representing the gradient of the succeeding layer.
+            lr (float): The learning rate for optimizer.
 
         Returns:
             in_delta (np.array): An array representing the gradient of this layer.
@@ -60,19 +59,19 @@ class SGD():
         batch_size = input.shape[0]
 
         ## Find the gradient of the activation function and multiply with error of the succeeding layer.
-        nabla_activation = self.activation.activateGrad(x)
+        nabla_activation = activation.activateGrad(x)
         delta = out_delta * nabla_activation
 
         ## Calucate the average change in weights over all the batches.
         nabla_w = input.T.dot(delta)
         nabla_w = nabla_w / batch_size
 
-        ## Find the average chaneg in biases over all the batches.
+        ## Find the average change in biases over all the batches.
         nabla_b = np.sum(delta, axis = 0) / batch_size
 
         ## Compute the updated weights and biases.
-        w_prime = weights - (self.lr * nabla_w)
-        b_prime = bias - (self.lr * nabla_b)
+        w_prime = weights - (lr * nabla_w)
+        b_prime = bias - (lr * nabla_b)
 
         ## Calculate the error of this layer.
         in_delta = delta.dot(weights.T)
