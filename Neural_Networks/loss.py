53251fc60a97dec11c3d29d306506f6ae29d9072
hoganchoi <131487154+hoganchoi@users.noreply.github.com>
2024-10-18 19:47:58 -0400

    Update loss.py
    Added MSE loss function.

--- Neural_Networks/loss.py
+++ Neural_Networks/loss.py
@@ -9,70 +9,71 @@ import cupy as cp
 ## Matrix operations are compiled on CPU.
 ##########################################################################################################################################################################
 
-## A class that represents the Mean Absolute Error compiled on the CPU.
-class MAE():
+## A class that represents the Mean Squared Error compiled on the CPU.
+class MSE():
     '''
-    A class that computes the Mean Absolute Error between the true value and the predicted value.
+    A class that computes the Mean Squared Error between the true value and the predicted value.
 
     Attributes:
-        actual_value (np.array): An array containing all the true values.
-        predictions (np.array): An array containing all the predictions from the model.
+        None
 
     Methods:
-        calculateLoss (self): Calculates the error between the true value and the model's predictions.
-        calculateLossGrad (self): Calculates the gradient of the error between the true value and the model's predictions.
+        calculateLoss (self, actual_value, predictions): Calculates the error between the true value and the model's predictions.
+        calculateLossGrad (self, actual_value, predictions): Calculates the gradient of the error between the true value and the model's predictions.
 
     Usage:
         A method to calculate the loss between the model and the actual values.
     '''
-    ## Initializes the MAE function on the CPU.
-    def __init__(self, true_outputs, model_outputs):
+    ## Initializes the MSE function on the CPU.
+    def __init__(self):
         '''
-        Initializes the parameters for MAE function.
-
-        Args:
-            true_outputs (np.array): An array containing all the true values.
-            model_outputs (np.array): An array containing all the model's predictions for each batch.
-
-        Returns:
-            None
-        '''
-        self.actual_value = true_outputs
-        self.predictions = model_outputs
-
-    ## Calculates the loss between the true value and the model's predictions on CPU.
-    def calculateLoss(self):
-        '''
-        Returns the average loss between the actual values and the model's predictions.
+        Initializes the parameters for MSE function.
 
         Args:
             None
 
         Returns:
-            mae (np.array): An array storing all the Mean Absolute Errors for each batch.
+            None
         '''
-        ## Calculate the Mean Absolute Error between the actual value and the model's predictions.
-        mae = np.mean(np.abs(self.actual_value - self.predictions), axis = 1)
-        ## Return the MAE value.
-        return mae
+        pass
+
+    ## Calculates the loss between the true value and the model's predictions using MSE on CPU.
+    def calculateLoss(self, actual_value, predictions):
+        '''
+        Returns the average MSE loss between the actual values and the model's predictions.
+
+        Args:
+            actual_value (np.array): An array containing all the true values.
+            predictions (np.array): An array containing all the predictions from the model.
+
+        Returns:
+            mse (np.array): An array storing all the Mean Squared Errors for each batch.
+        '''
+        ## Calculate the Mean Squared Error between the actual value and the model's predictions.
+        mse_batch = np.mean((actual_value - predictions)**2, axis = 1)
+        mse = np.mean(mse_batch)
+        ## Return the MSE value.
+        return mse
     
-    ## Computes the gradient of the MAE loss function.
-    def calculateLossGrad(self):
+    ## Computes the gradient of the MSE loss function.
+    def calculateLossGrad(self, actual_value, predictions):
         '''
-        Returns the gradient of the loss function compiled on CPU.
+        Returns the gradient of the MSE loss function compiled on CPU.
 
         Args:
-            None
-
+            actual_value (np.array): An array containing all the true values.
+            predictions (np.array): An array containing all the predictions from the model.
+            
         Returns:
-            nabla_mae (np.array): An array storing all the gradient error values for each batch.
+            delta (np.array): An array storing all the gradient error values for each batch w.r.t. the activation function.
         '''
         ## Find the number of elements in the actual value vector.
-        num_elements = self.actual_value.shape[0]
+        num_elements = actual_value.shape[1]
         ## Calculate the gradient of the MAE function.
-        nabla_mae = (-1 / num_elements) * (np.sign(self.actual_value - self.predictions))
-        ## Return the gradient of MAE.
-        return nabla_mae
+        nabla_mse = (2 / num_elements) * (predictions - actual_value)
+
+        ## Return the gradient of MSE w.r.t. the activation function of the output layer.
+        return nabla_mse
 
 ##########################################################################################################################################################################
 ## Neural Network loss functions created using CuPy.
